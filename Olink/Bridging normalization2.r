880517f863f2a02c5536fc5e0615d3318d80d76b
StringAech <guohao@yucebio.com>
2023-06-06 17:21:21 +0800

    将原本的三个结果文件写入同一个文件不同sheet内

--- Olink/Bridging normalization2.r
+++ Olink/Bridging normalization2.r
@@ -1,6 +1,6 @@
 library(OlinkAnalyze)
-npx_df1_path <- "./1/sampleid_20230313-IO-YKKY0091_NPX.xlsx"
-npx_df2_path <- "./2/sampleid_YKKY0091_IO_NPX.xlsx"
+npx_df1_path <- "./results/sampleid_CDM205YTP0018(微芯-51个样本)_NPX_long.xlsx"
+npx_df2_path <- "./results/sampleid_微芯单独样本_NPX.xlsx"
 npx_df1 <- read.xlsx(npx_df1_path) %>%
     dplyr::mutate(Project = "P1")
 npx_df2 <- read.xlsx(npx_df2_path) %>%
@@ -8,8 +8,12 @@ npx_df2 <- read.xlsx(npx_df2_path) %>%
 overlap_samples <- intersect((npx_df1 %>%
     dplyr::filter(!grepl("control", SampleID, ignore.case = TRUE)))$SampleID, (npx_df2 %>%
     dplyr::filter(!grepl("control", SampleID, ignore.case = TRUE)))$SampleID)
-NPX_normalization <- olink_normalization(df1 = npx_df1, df2 = npx_df2, overlapping_samples_df1 = overlap_samples, df1_project_nr = "P1", df2_project_nr = "P2",
-    reference_project = "P1") %>%
+NPX_normalization <- olink_normalization(df1 = npx_df1, 
+                                         df2 = npx_df2, 
+                                         overlapping_samples_df1 = overlap_samples, 
+                                         df1_project_nr = "P1", 
+                                         df2_project_nr = "P2",
+                                         reference_project = "P1") %>%
     dplyr::filter(., !.$SampleID == "Samplecontrol")
 
 NPX_normalization_exp <- tidyr::pivot_wider(NPX_normalization[, c("SampleID", "Assay", "NPX", "Project")], names_from = "Assay", values_from = "NPX") %>%
@@ -26,6 +30,11 @@ attach(NPX_normalization_matrix)
 NPX_normalization_matrix_p1 <- dplyr::filter(NPX_normalization_matrix, Project == "P1")
 NPX_normalization_matrix_p2 <- dplyr::filter(NPX_normalization_matrix, Project == "P2")
 detach(NPX_normalization_matrix)
-write.xlsx(NPX_normalization_matrix, paste0(dirname(npx_df1_path), "/NPX_normalization_matrix_", gsub(basename(npx_df1_path), pattern = "sampleid_", replacement = "")))
-write.xlsx(NPX_normalization_matrix_p1, paste0(dirname(npx_df1_path), "/NPX_normalization_matrix_P1_", gsub(basename(npx_df1_path), pattern = "sampleid_", replacement = "")))
-write.xlsx(NPX_normalization_matrix_p2, paste0(dirname(npx_df1_path), "/NPX_normalization_matrix_P2_", gsub(basename(npx_df1_path), pattern = "sampleid_", replacement = "")))
+# write.xlsx(NPX_normalization_matrix, paste0(dirname(npx_df1_path), "/NPX_normalization_matrix_", gsub(basename(npx_df1_path), pattern = "sampleid_", replacement = "")))
+# write.xlsx(NPX_normalization_matrix_p1, paste0(dirname(npx_df1_path), "/NPX_normalization_matrix_P1_", gsub(basename(npx_df1_path), pattern = "sampleid_", replacement = "")))
+# write.xlsx(NPX_normalization_matrix_p2, paste0(dirname(npx_df1_path), "/NPX_normalization_matrix_P2_", gsub(basename(npx_df1_path), pattern = "sampleid_", replacement = "")))
+write.xlsx(list(matrix=NPX_normalization_matrix,
+                matrix_p1=NPX_normalization_matrix_p1,
+                matrix_p2=NPX_normalization_matrix_p2),
+           paste0(dirname(npx_df1_path), "/NPX_normalization_matrix_", gsub(basename(npx_df1_path), pattern = "sampleid_", replacement = ""))
+           )
